e64fa815261caf415388f3c54233c64ec16ea935
CKATEPTb <taksis@ex.ua>
2022-12-03 16:17:23 +0200

    upload

---
+++ src/main/java/dev/ckateptb/minecraft/caffeine/Caffeine.java
@@ -0,0 +1,10 @@
+package dev.ckateptb.minecraft.caffeine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Caffeine extends JavaPlugin {
+    // The plugin includes the Caffeine library,
+    // which manages all the logic. The plugin does not need
+    // additional code. The plugin itself is necessary so as not
+    // to duplicate the library.
+}
